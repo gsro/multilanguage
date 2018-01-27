f7e61c8d6a049929fbfbf728493eb9081b5df461
Gabi DJ <gabi@apidemia.com>
2018-01-27 19:01:20 +0200

    updated sql export

--- data/multilanguage.sql
+++ data/multilanguage.sql
@@ -1,60 +1,32 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 26, 2018 at 01:50 PM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.1.7
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `workshop`
---
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.21-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.4.0.5125
 -- --------------------------------------------------------
 
---
--- Table structure for table `multilanguage`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `multilanguage` (
-  `id` int(11) NOT NULL,
-  `languageCode` varchar(10) NOT NULL INDEX,
-  `name` varchar(150) NOT NULL INDEX,
-  `value` text NOT NULL
+-- Dumping structure for table cryptos.multilanguage
+CREATE TABLE IF NOT EXISTS `multilanguage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `languageCode` varchar(20) DEFAULT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `value` varchar(2000) DEFAULT NULL,
+  `dateAdded` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`),
+  KEY `languageCode` (`languageCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
+-- Dumping data for table cryptos.multilanguage: ~0 rows (approximately)
+/*!40000 ALTER TABLE `multilanguage` DISABLE KEYS */;
+/*!40000 ALTER TABLE `multilanguage` ENABLE KEYS */;
 
---
--- Indexes for table `multilanguage`
---
-ALTER TABLE `multilanguage`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `multilanguage`
---
-ALTER TABLE `multilanguage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
